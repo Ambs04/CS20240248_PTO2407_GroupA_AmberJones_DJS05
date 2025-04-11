e94398a4039f75eb06ea283be8f7ed49acbf8a69
Ambs04 <amberleejones2007@gmail.com>
2025-04-11 21:05:21 +0200

    create reducer function

--- script.js
+++ script.js
@@ -12,3 +12,13 @@
 {
   type: "RESET";
 }
+
+function reducer(state = 0, action) {
+  if (action.type === "ADD") {
+    return state + 1;
+  } else if (action.type === "SUBTRACT") {
+    return state - 1;
+  } else if (action.type === "RESET") {
+    return (state = 0);
+  }
+}
