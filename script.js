74bffee16c71b8c4a091ba077c406be72dba2aae
Ambs04 <amberleejones2007@gmail.com>
2025-04-11 21:47:48 +0200

    fix validation logic so tally never goes into negatives

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ export default function reducer(state = 0, action) {
   if (action.type === "ADD") {
     return state + 1;
   } else if (action.type === "SUBTRACT") {
-    return state - 1;
+    return state > 0 ? state - 1 : state;
   } else if (action.type === "RESET") {
     return (state = 0);
   }
